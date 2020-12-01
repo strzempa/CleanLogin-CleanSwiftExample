9910aa6741ff97d721711b81fcb7cb9d725dc64f
Patryk Strzemiecki <strzempa@wp.pl>
2020-12-01 09:20:16 +0100

    Initial Commit

---
+++ CleanLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanLogin
+//
+//  Created by Patryk Strzemiecki on 01/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
